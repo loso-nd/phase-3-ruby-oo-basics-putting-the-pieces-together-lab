a20c4f8b3412c9eb283fefbefa5efb95b8b6bf4f
Nahum <losond8@gmail.com>
2021-07-08 21:29:06 -0400

    commit tree bf75204a248699077e94dac402a7dc9dc597715c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+
+    attr_reader :title
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
+#Note:
+
+# ::new - gets initialized with attr_reader
+
+# properties - means we add attr_accessor
+
+# #turn_page - instance method
